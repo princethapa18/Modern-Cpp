3efd4f81571c64ca058074167dd35ac4c78fb165
Prince Thapa <princethapa11@protonmail.com>
2021-08-06 19:16:36 +0530

    Update lambda.cpp

--- lambda.cpp
+++ lambda.cpp
@@ -30,9 +30,54 @@
    
 */
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
+typedef enum FUNTYPE
+{   
+    FUNCTIONS = 1,
+    VECTORS,
+    TENSORS
+} FUNTYPE;
+
+typedef struct FunctionInfo
+{
+    int funtype;
+    std::string funname;
+    FunctionInfo(int ftype, std::string fname) : funtype(ftype) , funname(fname) {}
+    FunctionInfo() : funtype(0) , funname("") {}
+} FunctionInfo;
+
+void addToFunctionList(FUNTYPE funtype, std::string fname, std::vector<FunctionInfo>& funInfoList )
+{
+    FunctionInfo funInfo{funtype, fname};
+    funInfoList.push_back(funInfo);
+}
+
+void printFunctionList(std::vector<FunctionInfo>& funInfoList)
+{
+    for(auto itr : funInfoList)
+    {
+        switch (itr.funtype)
+        {
+        case FUNTYPE::FUNCTIONS:
+            cout << "S: " << itr.funname << endl;
+            break;
+        case FUNTYPE::VECTORS:
+            cout << "V: " << itr.funname << endl;
+            break;
+        case FUNTYPE::TENSORS:
+            cout << "T: " << itr.funname << endl;
+            break;      
+        default:
+            cout << "Invalid function " << endl;
+            break;
+        }        
+    }
+}
 
 void test()
 {
@@ -84,8 +129,6 @@ void test()
         std::cout << "i : " << i << " d: " << d << " f: " << f << std::endl ; 
     };
     l_mixed(); // O/P  i : 90 d: 20.7 f: 4.5     f is incremented by 1
-
-
 }
 
 int main()
@@ -109,6 +152,31 @@ int main()
         var++; 
         lambda_ref();     // prints 46, 47, 48
     }
-
     test();
+
+    // Using a lambda as a compare function for sorting
+    std::vector<FunctionInfo> funInfoList; // added in random order
+    addToFunctionList(FUNTYPE::FUNCTIONS, "Moment in local X", funInfoList);
+    addToFunctionList(FUNTYPE::VECTORS, "Displacement", funInfoList);
+    addToFunctionList(FUNTYPE::VECTORS, "Acceleration", funInfoList);
+    addToFunctionList(FUNTYPE::FUNCTIONS, "Moment in local X", funInfoList);
+    addToFunctionList(FUNTYPE::TENSORS, "Stress Tensor", funInfoList);
+    addToFunctionList(FUNTYPE::FUNCTIONS, "Displacement in X", funInfoList);
+    addToFunctionList(FUNTYPE::FUNCTIONS, "Displacement in Y", funInfoList);
+    addToFunctionList(FUNTYPE::TENSORS, "Strain Tensor", funInfoList);
+    addToFunctionList(FUNTYPE::FUNCTIONS, "Principal major stress", funInfoList);
+
+    std::cout << "********* funInfoList before sort ************\n";
+    printFunctionList(funInfoList);
+
+    std::sort(funInfoList.begin(), funInfoList.end(), 
+              [](const FunctionInfo& a, const FunctionInfo& b)
+              {
+                  if(a.funtype == b.funtype)
+                    return a.funname < b.funname;
+                  return a.funtype < b.funtype;
+              } );
+
+    std::cout << "********* funInfoList after sort ************\n";
+    printFunctionList(funInfoList);              
 }
